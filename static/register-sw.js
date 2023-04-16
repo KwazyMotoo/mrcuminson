2f25229c8299b5cc6b3a7505fbbf407766675e8b
Kwazy <124801774+KwazyMotoo@users.noreply.github.com>
2023-04-16 16:18:48 -0500

    Update register-sw.js

--- static/register-sw.js
+++ static/register-sw.js
@@ -1,3 +1,13 @@
-navigator.serviceWorker.register("/static/uv-sw.js", {
-    scope: __uv$config.prefix
-});
+"use strict";
+/**
+ * Global util
+ * Used in index.html
+ */
+async function registerSW() {
+  if (!navigator.serviceWorker)
+    throw new Error("Your browser doesn't support service workers.");
+
+  await navigator.serviceWorker.register("/static/uv-sw.js", {
+    scope: __uv$config.prefix,
+  });
+}
