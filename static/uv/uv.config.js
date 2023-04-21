84f0798ac36dc45afdcea4fd9df1b33af9cf8425
BenRogozinski <brogodev@gmail.com>
2023-04-21 14:36:20 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/uv/service/',
-    bare: 'https://node.starttiw.org/',
+    bare: 'http://node.starttiw.org/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
