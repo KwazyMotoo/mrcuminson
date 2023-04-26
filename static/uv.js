655d951ab4947a201bb4754b73dfa4c7c3f4c4c7
Kwazy <124801774+KwazyMotoo@users.noreply.github.com>
2023-04-25 19:49:50 -0500

    Update uv.js

--- static/uv.js
+++ static/uv.js
@@ -1,6 +1,6 @@
-importScripts('/js/uv/uv.bundle.js');
-importScripts('/js/uv/uv.config.js');
-importScripts('/js/uv/uv.sw.js');
+importScripts('/static/uv/uv.bundle.js');
+importScripts('/static/uv/uv.config.js');
+importScripts('/static/uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
